e40f3eaf62451c8026e95d62f2e69597d1e36cff
João Vitor <joaovitormcz16@gmail.com>
2025-06-07 22:52:03 -0300

    Coloquei as imagens corretas das pulseiras

--- catalogo_pulseiras.php
+++ catalogo_pulseiras.php
@@ -4,13 +4,13 @@
     // Produtos fictícios para pulseiras
     $produtos_pulseiras = [
         ["id" => 17, "nome" => "Pulseira Cordão Baiano Torcido", "preco" => "135,00", "imagem" => "img/Pulseira 18cm Cordão Baiano 2.0 Torcido.png", "descricao" => "Um clássico de brilho e sofisticação para seu pulso."],
-        ["id" => 18, "nome" => "Pulseira de Flor", "preco" => "168,00", "imagem" => "https://placehold.co/300x300/FCEFB4/666?text=Pulseira+Berloques", "descricao" => "Conte sua história com nossos berloques exclusivos."],
-        ["id" => 19, "nome" => "Pulseira Dupla Corações Pérolas e Vazados", "preco" => "117,00", "imagem" => "https://placehold.co/300x300/FAE588/555?text=Bracelete+Liso", "descricao" => "Design minimalista e elegante para o dia a dia."],
-        ["id" => 20, "nome" => "Pulseira Olho Grego Proteção", "preco" => "159,90", "imagem" => "https://placehold.co/300x300/FDF8E1/777?text=Pulseira+Olho+Grego", "descricao" => "Amuleto de sorte e proteção com muito estilo."],
-        ["id" => 21, "nome" => "Pulseira Infantil Delicada", "preco" => "198,00", "imagem" => "https://placehold.co/300x300/FCEFB4/666?text=Pulseira+Infantil", "descricao" => "Fofura e delicadeza para as pequenas princesas."],
-        ["id" => 22, "nome" => "Mix de Pulseiras Boho", "preco" => "189,00", "imagem" => "https://placehold.co/300x300/FAE588/555?text=Mix+Pulseiras", "descricao" => "Conjunto estiloso para um look despojado e moderno."],
-        ["id" => 23, "nome" => "Pulseira Masculina Couro", "preco" => "135,00", "imagem" => "https://placehold.co/300x300/E1E8FD/444?text=Pulseira+Couro", "descricao" => "Estilo robusto e moderno para homens de atitude."],
-        ["id" => 24, "nome" => "Pulseira Pérolas Clássica", "preco" => "168,00", "imagem" => "https://placehold.co/300x300/FDF8E1/888?text=Pulseira+Pérolas", "descricao" => "Elegância atemporal com pérolas delicadas."],
+        ["id" => 18, "nome" => "Pulseira de Flor", "preco" => "168,00", "imagem" => "img/Pulseira de Flor.jpg", "descricao" => "Conte sua história com nossos berloques exclusivos."],
+        ["id" => 19, "nome" => "Pulseira Dupla Corações Pérolas e Vazados", "preco" => "117,00", "imagem" => "img/Pulseira Dupla Corações Pérolas e Vazados.jpg", "descricao" => "Design minimalista e elegante para o dia a dia."],
+        ["id" => 20, "nome" => "Pulseira Elos 3x1 Fina", "preco" => "159,90", "imagem" => "img/Pulseira Elos 3x1 Fina.jpg", "descricao" => "Amuleto de sorte e proteção com muito estilo."],
+        ["id" => 21, "nome" => "Pulseira Elos Duplix", "preco" => "198,00", "imagem" => "img/Pulseira Elos Duplix 15cm + Extensor Fecho LV.jpg", "descricao" => "Fofura e delicadeza para as pequenas princesas."],
+        ["id" => 22, "nome" => "Pulseira Esferas e Cristais", "preco" => "189,00", "imagem" => "img/Pulseira Esferas e Cristais 18cm + Extensor 5cm.png", "descricao" => "Conjunto estiloso para um look despojado e moderno."],
+        ["id" => 23, "nome" => "Pulseira Rede Oca", "preco" => "135,00", "imagem" => "img/Pulseira Rede Oca.jpg", "descricao" => "Estilo robusto e moderno para homens de atitude."],
+        ["id" => 24, "nome" => "Pulseira Tripla Pontos De Luz Zircônias", "preco" => "168,00", "imagem" => "img/Pulseira tripla pontos de luz zircônias.jpg", "descricao" => "Elegância atemporal com pérolas delicadas."],
     ];
 ?>
 
